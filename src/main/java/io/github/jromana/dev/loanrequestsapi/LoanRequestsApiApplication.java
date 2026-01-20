ab726515b33a3e6f604ff82519f8d1b0d9069693
jromana-dev <javier.romana.dev@gmail.com>
2026-01-20 11:34:43 +0100

    Initial Spring Boot project setup
    Set up a new Spring Boot project for the loan-requests-api application. Includes Maven wrapper scripts, project configuration (pom.xml), application entry point, basic test, and default properties.

---
+++ src/main/java/io/github/jromana/dev/loanrequestsapi/LoanRequestsApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.jromana.dev.loanrequestsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanRequestsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanRequestsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jromana/dev/loanrequestsapi/LoanRequestsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jromana.dev.loanrequestsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanRequestsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
